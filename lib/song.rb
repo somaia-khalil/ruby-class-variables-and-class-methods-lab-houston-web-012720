d2c7d05e8d97bb3634dde345946c6cd47edb53b2
Somaia Khalil <sumaya_khair@live.com>
2020-01-30 13:59:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   attr_accessor :name, :artist , :genre
   def initialize(name, artist , genre)
     @name = name
-    @artists = artist
+    @artist = artist
     @genre = genre
     @@count +=1
     @@artist << artist 
